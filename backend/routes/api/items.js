7e47710959dea93f4fb53aae21a9740afb1c46a2
Ishay Melcer <ishaymelcer@gmail.com>
2022-07-10 19:17:38 +0300

    fix

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -87,7 +87,7 @@ router.get("/", auth.optional, function(req, res, next) {
         req.payload ? User.findById(req.payload.id) : null
       ]).then(async function(results) {
         var items = results[0].filter(item => {
-          return item.title===title;
+          return item.title.includes(title);
       });
         var itemsCount = items.length;
         var user = results[2];
